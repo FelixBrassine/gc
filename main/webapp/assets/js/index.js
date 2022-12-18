61ef2ce145eb165457409bea50bb9ed4ae0ab05c
Felix Brassine <brassine1987@gmail.com>
2022-12-18 15:00:56 +0100

    push css review2

--- main/webapp/assets/js/index.js
+++ main/webapp/assets/js/index.js
@@ -1,10 +1,14 @@
 let gameSelected = false;
 let cardsDealt = false;
 let nbPlayer;
+let countClick = 0;
 
 function playWar(){
-    start();
     nbPlayer = 2;
+    start(nbPlayer);
+    console.log("playwar");
+    console.log(nbPlayer);
+    getsuitRank();
     let imgDeck = document.getElementById("imgDeck");
     imgDeck.setAttribute("onclick", "getsuitRank()");
 
@@ -12,7 +16,9 @@ function playWar(){
 
 function getsuitRank(){
     if (nbPlayer>0) {
-        nbPlayer -= 1;
+        countClick ++;
+        console.log("get1");
+        console.log(nbPlayer);
         var xhr = new XMLHttpRequest(),
             method = "GET",
             url = "/gc/cardsDealt";
@@ -20,54 +26,60 @@ function getsuitRank(){
         xhr.onreadystatechange = function () {
             if(xhr.readyState === 4 && xhr.status === 200) {
                 console.log(xhr.responseText);
-                let game = document.getElementById("game");
-                let cardFace = document.createElement("div");
-                let imgCardFace = document.createElement("img");
-
-                cardFace.setAttribute("class", "cardFace")
-                game.appendChild(cardFace);
-                cardFace.appendChild(imgCardFace);
-                imgCardFace.setAttribute("class", "imgCardFace");
+                let imgCardFace = document.getElementById("imgCardFace" +countClick);
                 imgCardFace.setAttribute("src","/gc/assets/image/card"+xhr.responseText+".svg");
             }
         };
         xhr.send();
+        nbPlayer -= 1;
+        console.log("get2");
+        console.log(nbPlayer);
     }else{
         cardsDealt = true;
     }
 
 }
 
-function start(){
+function start(nbPlayer){
     if (gameSelected) {
         return;
     }
     let main = document.getElementById("main");
-        let board = document.createElement("div");
-            let topBoard = document.createElement("div");
-                let deck = document.createElement("div");
-                let imgDeck = document.createElement("img");
-                let treat = document.createElement("div");
-                let imgTreat = document.createElement("img");
-            let game = document.createElement("div");
+    let board = document.createElement("div");
+    let topBoard = document.createElement("div");
+    let deck = document.createElement("div");
+    let imgDeck = document.createElement("img");
+    let treat = document.createElement("div");
+    let imgTreat = document.createElement("img");
+    let game = document.createElement("div");
 
-        board.setAttribute("id","board");
-            topBoard.setAttribute("id","topBoard");
-                deck.setAttribute("id","deck");
-                imgDeck.setAttribute("id", "imgDeck");
-                imgDeck.setAttribute("src", "/gc/assets/image/cardBack.png")
-                treat.setAttribute("id","treat");
-                imgTreat.setAttribute("id", "imgTreat");
-                imgTreat.setAttribute("src", "/gc/assets/image/cardBack.png")
-            game.setAttribute("id","game");
+    board.setAttribute("id","board");
+    topBoard.setAttribute("id","topBoard");
+    deck.setAttribute("id","deck");
+    imgDeck.setAttribute("id", "imgDeck");
+    imgDeck.setAttribute("src", "/gc/assets/image/cardBack.png");
+    treat.setAttribute("id","treat");
+    imgTreat.setAttribute("id", "imgTreat");
+    game.setAttribute("id","game");
 
     main.appendChild(board);
-        board.appendChild(topBoard);
-            topBoard.appendChild(deck);
-                deck.appendChild(imgDeck);
-            topBoard.appendChild(treat);
-                treat.appendChild(imgTreat);
-        board.appendChild(game);
+    board.appendChild(topBoard);
+    topBoard.appendChild(deck);
+    deck.appendChild(imgDeck);
+    topBoard.appendChild(treat);
+    treat.appendChild(imgTreat);
+    board.appendChild(game);
+    for (let i = 1 ; i<=nbPlayer ; i ++){
+        let cardFace = document.createElement("div");
+        let imgCardFace = document.createElement("img");
+        cardFace.setAttribute("class", "cardFace");
+        imgCardFace.setAttribute("class", "imgCardFace");
+        cardFace.setAttribute("id", "cardFace" + i);
+        imgCardFace.setAttribute("id", "imgCardFace" + i);
+        game.appendChild(cardFace);
+        cardFace.appendChild(imgCardFace);
+        console.log("start");
+        console.log(nbPlayer);
+    }
     gameSelected = true;
 }
-
